d175a48b29283302d59f18d11fd05cb7f2f31c58
glau-isidoro <glaucia.isidoro@gmail.com>
2016-09-03 16:19:52 -0300

    User create jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,4 +4,20 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_parameters)
+    @job.save
+    redirect_to @job
+  end
+
+  private
+
+  def job_parameters
+    params.require(:job).permit(:title, :location, :category, :company, :description)
+  end
+
 end
